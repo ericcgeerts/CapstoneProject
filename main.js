07d11060577fc6ab50fc044099fd2ce0f79703e6
Eric Connelly <43161744+ericcgeerts@users.noreply.github.com>
2025-01-20 00:49:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const pythonProcess = spawn('python', ['final_capstone_algorithm.py'])
 
 //generic electron boilerplate code
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -37,50 +37,49 @@ app.on('window-all-closed', () => {
 
 
 //Grab values from Text input and pass to a CSV so that when we call python, it can read them
-ipcMain.on('text-input', (event, inputValue,inputValue1,inputValue2,inputValue3) => {
-    
-    //console.log(`Received input: ${inputValue}`);
-   // console.log(`Received input: ${inputValue1}`);
-    //console.log(`Received input: ${inputValue2}`);
-    //console.log(`Received input: ${inputValue3}`); 
-    
-    const array = [];
-    array.push(inputValue, inputValue1,inputValue2,inputValue3);
-    console.log(`Received input: ${array}`)
+ipcMain.on('text-input', (event, inputValue, inputValue1, inputValue2, inputValue3) => {
 
-    const result = array.join(',');
+  //console.log(`Received input: ${inputValue}`);
+  // console.log(`Received input: ${inputValue1}`);
+  //console.log(`Received input: ${inputValue2}`);
+  //console.log(`Received input: ${inputValue3}`); 
 
-    fs.writeFile('Weights.csv', result, (err) => {
-      if (err) {
-        console.error('Error writing to CSV file', err);
-      } 
-    });
-  }) 
-  //Handle Student Data Upload
-  ipcMain.handle('select-file', async () => {
-    const result = await dialog.showOpenDialog({
-        properties: ['openFile'], // Allow file selection
-    })
+  const array = [];
+  array.push(inputValue, inputValue1, inputValue2, inputValue3);
+  //console.log(`Received input: ${array}`)
 
-    if (result.canceled) {
-        return null; // User canceled the dialog
+  const result = array.join(',');
+
+  fs.writeFile('Weights.csv', result, (err) => {
+    if (err) {
+      console.error('Error writing to CSV file', err);
     }
+  });
+})
+//Handle Student Data Upload
+ipcMain.handle('select-file', async () => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile'], // Allow file selection
+  })
 
-    // Return the file path and save in a .txt so that it can be passed to python algorithim
-    fs.writeFile('FilePath.txt', result, (err) => {
-      if (err) {
-        console.error('Error writing to txt file', err);
-      } })
+  if (result.canceled) {
+    return null; // User canceled the dialog
+  }
+
+  // Return the file path and save in a .txt so that it can be passed to python algorithim
+  fs.writeFile('FilePath.txt', result, (err) => {
+    if (err) {
+      console.error('Error writing to txt file', err);
+    }
+  })
 })
 
 //On Submit, call the python algorithim after saving the student data file path that was uploaded
 // No need to pass data as it is saved locally in a CSV
 ipcMain.on('results-input', (event, inputValue) => {
-   // console.log(`Received input: ${inputValue}`);
-    const dataPython = ''
-    pythonProcess.stdout.on('data', (dataPython) => {
-      //console.log(`Output: ${dataPython}`);
+  // console.log(`Received input: ${inputValue}`);
+  const dataPython = ''
+  pythonProcess.stdout.on('data', (dataPython) => {
+    //console.log(`Output: ${dataPython}`);
   });
-}) 
-
-//
+})
